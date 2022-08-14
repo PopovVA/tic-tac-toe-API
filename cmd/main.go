9b2ba6bba38be55883fe4cb4c55a91de09676a2b
Vadim Popov <m.vadimpopov@gmail.com>
2022-08-14 18:38:34 +0700

    feat: add config and migrations

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,31 @@ import (
 	"github.com/PopovVA/tic-tac-toe-API/pkg/handler"
 	"github.com/PopovVA/tic-tac-toe-API/pkg/repository"
 	"github.com/PopovVA/tic-tac-toe-API/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
-	srv := new(tictactoeapi.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
-		log.Fatalf("Unable to run the server, %s", err.Error())
+	port := viper.GetString("port")
+	if len(port) == 0 {
+		log.Fatal("error reading the port")
 	}
+
+	srv := new(tictactoeapi.Server)
+	if err := srv.Run(port, handlers.InitRoutes()); err != nil {
+		log.Fatalf("unable to run the server, %s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
